66c0c09cbec82df54d15a7ea8e9e127926075c23
Natalia Kuzmicheva <kuzmichyova.natalie@yandex.ru>
2024-04-27 14:43:32 +0300

    Add homework

---
+++ src/main/java/NotRegisteredException.java
@@ -0,0 +1,9 @@
+public class NotRegisteredException extends RuntimeException {
+
+    public NotRegisteredException(String name) {
+        super(
+                "Player with name " + name + "not registered"
+        );
+    }
+}
+
